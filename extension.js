59f09caa2dff8dbd70b05560ea4e38f95a71593e
eexpress <eexpress@163.com>
2022-01-31 07:33:20 +0800

    add menu for file/dir

--- extension.js
+++ extension.js
@@ -17,16 +17,23 @@ const PopupMenu = imports.ui.popupMenu;
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
     _init() {
-        super._init(0.0, _('My Shiny Indicator'));
+        super._init(0.0, _('Compare Dir/File'));
 
         this.add_child(new St.Icon({
             icon_name: 'tools-check-spelling-symbolic',
             style_class: 'system-status-icon',
         }));
 
-        let item = new PopupMenu.PopupMenuItem(_('Compare last two Dirs/Files in Clipboard.'));
+        let item = new PopupMenu.PopupMenuItem(_('Compare this two Dirs/Files below.'));
         item.connect('activate', () => { comp(); });
         this.menu.addMenuItem(item);
+
+        let item0 = new PopupMenu.PopupMenuItem('1: ');
+        item0.reactive = false;
+        this.menu.addMenuItem(item0);
+        let item1 = new PopupMenu.PopupMenuItem('2: ');
+        item1.reactive = false;
+        this.menu.addMenuItem(item1);
 
 		this._selection = global.display.get_selection();
 		this._clipboard = St.Clipboard.get_default();
@@ -38,7 +45,9 @@ class Indicator extends PanelMenu.Button {
 						if(file.indexOf(clip0) == -1){	//new file
 							file.push(clip0);
 							file = file.slice(-2);
-							lg(file);
+							item0.label.text = "1: "+(file[0] || " ");
+							item1.label.text = "2: "+(file[1] || " ");
+							//~ lg(file);
 						}
 					}
 				}
@@ -46,14 +55,20 @@ class Indicator extends PanelMenu.Button {
 		});
 
 		function comp(){
+			if(file.length < 2){
+				Main.notify(_("Need copy 2 Dirs/Files before compare."));
+				return 1;
+			}
 			const f0 = GLib.file_test(file[0], GLib.FileTest.IS_DIR)?1:0;
 			const f1 = GLib.file_test(file[1], GLib.FileTest.IS_DIR)?1:0;
 			if(f0 != f1){
-				Main.notify(_("2 filetype is defferent."));
+				Main.notify(_("Different file types."));
 				return 1;
 			}
 			GLib.spawn_command_line_async('meld "%s" "%s"'.format(file[0], file[1]));
 			file = [];
+			item0.label.text = "1: ";
+			item1.label.text = "2: ";
 		};
     }
 });
